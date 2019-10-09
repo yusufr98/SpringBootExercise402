641062af583feb20374c087298caf285641ff0e7
yusufr98 <yusuf090798@gmail.com>
2019-10-09 11:52:35 -0400

    intial commit

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+                .authorizeRequests().antMatchers("/css/**", "/js/**").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
